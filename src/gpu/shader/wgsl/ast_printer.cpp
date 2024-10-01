e347012e785646e253032efee37c6e9b8c3dd512
Kirill <gavialhim1994@gmail.com>
2024-10-01 12:52:11 +0300

    [wgsl] ast_printer temp fix

--- src/gpu/shader/wgsl/ast_printer.cpp
+++ src/gpu/shader/wgsl/ast_printer.cpp
@@ -22,42 +22,6 @@ void AstPrinter::PrintConstVar(const ConstVariable* node) {
 }
 
 void AstPrinter::PrintExpression(const Expression* node) {
-    if(auto expr = node->As<BinaryExpression>()) {
-        TreePrinter::AutoObject a(printer_, "binary_expression");
-        printer_->Entry("op", to_string(expr->op));
-
-        printer_->OpenObject("lhs");
-        PrintExpression(expr->lhs);
-        printer_->CloseObject();
-
-        printer_->OpenObject("rhs");
-        PrintExpression(expr->rhs);
-        printer_->CloseObject();
-        return;
-    }
-    if(auto expr = node->As<UnaryExpression>()) {
-        TreePrinter::AutoObject u(printer_, "unary_expression");
-        printer_->Entry("op", to_string(expr->op));
-
-        printer_->OpenObject("rhs");
-        PrintExpression(expr->rhs);
-        printer_->CloseObject();
-        return;
-    }
-    if(auto literal = node->As<LiteralExpression>()) {
-        if(auto iLiteral = node->As<IntLiteralExpression>()) {
-            TreePrinter::AutoObject i(printer_, "int_literal");
-            printer_->Entry("type", to_string(iLiteral->type->kind));
-            printer_->Entry("value", iLiteral->value);
-        } else if(auto fLiteral = node->As<FloatLiteralExpression>()) {
-            TreePrinter::AutoObject f(printer_, "float_literal");
-            printer_->Entry("type", to_string(fLiteral->type->kind));
-            printer_->Entry("value", fLiteral->value);
-        } else if(auto bLiteral = node->As<BoolLiteralExpression>()) {
-            TreePrinter::AutoObject b(printer_, "bool_literal");
-            printer_->Entry("value", bLiteral->value ? "true" : "false");
-        }
-    }
 }
 
 } // namespace wgsl::ast
